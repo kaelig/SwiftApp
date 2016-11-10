92bba47cb46f4171544381d25e720975969d4fc3
Kaelig <kaelig@deloumeau.fr>
2016-11-10 11:13:21 -0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+`SwiftApp` adheres to [Semantic Versioning](http://semver.org/).
+
+## [Master](https://github.com/kaelig/SwiftApp)
+### Added
+
+### Changed
+
+### Removed
---
+++ Source/SwiftApp.h
@@ -0,0 +1,19 @@
+//
+//  SwiftApp.h
+//  SwiftApp
+//
+//  Created by Kaelig on Nov 10, 2016.
+//  Copyright © 2016 Awesome Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftApp.
+FOUNDATION_EXPORT double SwiftAppVersionNumber;
+
+//! Project version string for SwiftApp.
+FOUNDATION_EXPORT const unsigned char SwiftAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftApp/PublicHeader.h>
+
+
